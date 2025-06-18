3a5ba49b2b9fcab804d735cc5b79032df749557a
Chae Myeong <qufslarkfn@naver.com>
2025-06-18 20:29:45 +0900

    커밋이 완료되었습니다.

--- src/main/java/com/CBTServer/WebCSAT/controller/WebApiController.java
+++ src/main/java/com/CBTServer/WebCSAT/controller/WebApiController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebApiController {
 
-    @GetMapping("/")
+    @GetMapping("/index")
     public String index() {
         return "hello world";
     }
