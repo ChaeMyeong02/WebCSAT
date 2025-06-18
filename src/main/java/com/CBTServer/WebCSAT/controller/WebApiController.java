e27463e4ad42c7878c7c06dc410f30ae72c9600a
Chae Myeong <qufslarkfn@naver.com>
2025-06-18 19:56:42 +0900

    커밋이 완료되었습니다.

--- src/main/java/com/CBTServer/WebCSAT/controller/WebApiController.java
+++ src/main/java/com/CBTServer/WebCSAT/controller/WebApiController.java
@@ -1,4 +1,4 @@
-package com.CBTServer.WebCSAT.Controller;
+package com.CBTServer.WebCSAT.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
