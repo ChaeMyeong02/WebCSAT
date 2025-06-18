ae2afe45e702c7fea9b15ca3be780d65fe724c78
Chae Myeong <qufslarkfn@naver.com>
2025-06-18 20:29:45 +0900

    커밋이 완료되었습니다.

--- src/main/java/com/CBTServer/WebCSAT/config/WebSecurityConfig.java
+++ src/main/java/com/CBTServer/WebCSAT/config/WebSecurityConfig.java
@@ -1,23 +1,40 @@
 package com.CBTServer.WebCSAT.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-//    @Bean
-//    public WebSecurityCustomizer configure() {
-//        return (web) -> web.ignoring()
-//                .requestMatchers("/");
-//    }
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/static/**");
+    }
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.csrf(csrf-> csrf.disable());
-//        http.authorizeHttpRequests(auth -> auth
-//                .requestMatchers("/login", "/signup", "/user").permitAll()
-//        );
-//        return http.build();
-//    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf-> csrf.disable());
+        http.formLogin(formLogin-> formLogin
+                .loginPage("/login")
+                .defaultSuccessUrl("/index")
+        );
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/login", "/signup", "/user").permitAll()
+                .anyRequest().authenticated()
+        );
+        http.logout(logout -> logout
+                .logoutSuccessUrl("/login"));
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder byCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
